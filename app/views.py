1b46c45972018fa690a047ab07316330bade8c99
Alfredo Arcia <arcia86alfredo@gmail.com>
2020-07-01 16:52:02 -0300

    initial commit save files

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+def home():
+   return "hello world!!!!"
+@app.route('/template')
+def template():
+    return render_template('home.html')
